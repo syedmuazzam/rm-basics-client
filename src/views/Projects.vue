a65ba28be28f5767f7ce50a1da18a9ef8673d412
syedmuazzam <30724260+syedmuazzam@users.noreply.github.com>
2020-10-10 10:26:50 +0530

    Update Projects.vue

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,17 +2,33 @@
   <div class="projects">
     <h3>Projects</h3>
     <v-container class="my-5">
-      content
+          <v-data-table
+            :headers="headers"
+            :items="projectNames"
+          >
+            <template v-slot: item.tasks="{item}">
+              {{item.tasks}}
+            </template>
+          </v-data-table>
     </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+// @ is an alias to /src 
 
 export default {
   name: 'Projects',
   components: {
+  },
+  computed: {
+    headers(){
+      return [
+      {text: 'Name', value: 'name'},
+      {text: 'Date', value: 'published_at'},
+      {text: 'No. of Tasks', value: 'tasks'}
+      ]
+    }
   }
 }
 </script>
